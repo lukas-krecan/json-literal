6593fa0ee94f68fac459a5fa11bc9e0997c4a8e5
Lukas Krecan <lukas@krecan.net>
2015-09-05 18:56:14 +0200

    Array test added

--- json-literal-test/src/main/java/net.javacrumbs.jsonliteral.test/AbstractJsonLiteralTest.java
+++ json-literal-test/src/main/java/net.javacrumbs.jsonliteral.test/AbstractJsonLiteralTest.java
@@ -28,8 +28,41 @@ public abstract class AbstractJsonLiteralTest {
                         bd -> new BigDecimal("1.23"),
                         bi -> new BigInteger("1")
                 )
-        ).isEqualTo("{\"one\": true, \"two\": {\"three\":false}, \"string\":\"value\", \"integer\":1, \"null\": null, \"dbl\": 1.1, " +
-                "\"flt\":1.0, \"arr\":[1, \"a\", 3], \"arr2\": [\"a\", \"b\", \"c\"], \"bd\": 1.23, \"bi\":1}");
+        ).isEqualTo("{\n" +
+                "   \"one\":true,\n" +
+                "   \"two\":{\n" +
+                "      \"three\":false\n" +
+                "   },\n" +
+                "   \"string\":\"value\",\n" +
+                "   \"integer\":1,\n" +
+                "   \"null\":null,\n" +
+                "   \"dbl\":1.1,\n" +
+                "   \"flt\":1.0,\n" +
+                "   \"arr\":[\n" +
+                "      1,\n" +
+                "      \"a\",\n" +
+                "      3\n" +
+                "   ],\n" +
+                "   \"arr2\":[\n" +
+                "      \"a\",\n" +
+                "      \"b\",\n" +
+                "      \"c\"\n" +
+                "   ],\n" +
+                "   \"bd\":1.23,\n" +
+                "   \"bi\":1\n" +
+                "}\n");
+    }
+
+    @Test
+    public void shouldCreateArrayOfNodes() {
+        JsonFluentAssert.assertThatJson(
+                obj(
+                        array -> asList(
+                                obj(a -> 1),
+                                obj(a -> 2)
+                        )
+                )
+        ).isEqualTo("{\"array\": [{\"a\":1}, {\"a\":2}]}");
     }
 
     protected abstract Object obj(NamedValue<Object>... keyValuePairs);
